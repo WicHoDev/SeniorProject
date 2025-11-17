5401803d186619b2018bc176c9d20af5addb72cb
WicHo <113863333+WicHoDev@users.noreply.github.com>
2025-11-16 23:16:10 -0600

    client update

--- DataTransfer/client.py
+++ DataTransfer/client.py
@@ -2,8 +2,17 @@ import socket
 
 #create client
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# client.connect(('129.113.131.75', 4444)) #school IP
+client.connect(('192.168.1.64', 4444))   #house IP
+receive = open("dataReceived.txt", "w")
 #local Host
-client.connect(('127.0.0.1', 4444))
+while True:
+    data = client.recv(1024).decode()
 
-client.send("Hello From Client".encode())
-print(client.recv(1024).decode())
+    print(data)
+    receive.write(data)
+
+    if data == "quit":
+        client.close()
+
+
