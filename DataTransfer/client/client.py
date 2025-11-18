ec779ae0b9253134bae7f074f1991b1a336af243
WicHo <113863333+WicHoDev@users.noreply.github.com>
2025-11-17 22:47:23 -0600

    UPDATE: created new file structure

--- DataTransfer/client/client.py
+++ DataTransfer/client/client.py
@@ -1,11 +1,14 @@
 import socket
+import pyvisa #read from USB
 
 #create client
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# client.connect(('129.113.131.75', 4444)) #school IP
-client.connect(('192.168.1.64', 4444))   #house IP
+#connect to server
+# client.connect(('129.113.131.75', 4444))  #school IP
+client.connect(('192.168.1.64', 4444))      #house IP
+
+
 receive = open("dataReceived.txt", "w")
-#local Host
 while True:
     data = client.recv(1024).decode()
 
--- DataTransfer/server/server.py
+++ DataTransfer/server/server.py
@@ -3,11 +3,11 @@ import socket
 #create socket
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(('0.0.0.0', 4444))
-data = open("data.txt", "r")
-
 server.listen(5)
-
 client, addr = server.accept()
+
+
+data = open("data.txt", "r")
 while client:
     i = data.read()
     client.send(i.encode())
