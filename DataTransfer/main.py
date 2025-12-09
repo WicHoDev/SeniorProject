12f14ee65ee1c98acc75470105a9e4259ec84226
WicHo <113863333+WicHoDev@users.noreply.github.com>
2025-12-09 17:40:56 -0600

    UPDATE

--- DataTransfer/main.py
+++ DataTransfer/main.py
@@ -1,1 +1,11 @@
+import mat
+import serial
+
 print("Hello world")
+
+ser = serial.Serial('COM3', 921600, timeout=1)
+print("connected: ", ser.name)
+
+ser.write(b"scan\r")
+raw = ser.read(4000)
+print(raw)
